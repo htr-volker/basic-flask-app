4b3e1bc4bbc02272b7a1c7862e78699805c22b90
htr-volker <htr.volker@gmail.com>
2019-12-19 09:39:55 +0000

    development

--- nginx/setup.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bash/env bash
-# install and configure nginx for the application stack
-# usage:
-#   ./setup.bash
-sudo apt update
-sudo apt install -y nginx
-sudo cp nginx.conf /etc/nginx/nginx.conf 
-sudo systemctl reload nginx
