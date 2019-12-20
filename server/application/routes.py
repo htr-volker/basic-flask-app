1a4b77a5c6a8ea105dc83b054551130eb37ae969
htr-volker <htr.volker@gmail.com>
2019-12-20 11:02:26 +0000

    Random number generation feature 2

--- server/application/routes.py
+++ server/application/routes.py
@@ -4,14 +4,15 @@ Python file that handles hyperlink routing within the site
 from flask import render_template, redirect, url_for, request
 from application import app, db
 from application.forms import GenerateNumberForm
+import requests
 
 # Route to home page
-@app.route('/')
+@app.route('/', methods = ["GET"])
 def home():
 	# button to generate a random number
 	generate_number = GenerateNumberButton()
+	# automatically request a random number
 	number = requests.get("http://localhost:5001/")
-
 	# request the number from service-1 when button is pressed
 	if generate_number.is_submitted():
 		number = requests.get("http://localhost:5001/")
--- service-1/application/routes.py
+++ service-1/application/routes.py
@@ -8,7 +8,7 @@ import requests
 
 @app.route('/', methods = ["GET"])
 def generate_random_number():
-	random_number = randint(0, 999)
-	requests.post('http://localhost:5000/', json = {'random_number' : random_number})
+	random_number = randint(0, 1000)
+	#requests.post('http://localhost:5000/', json = {'random_number' : random_number})
 	print("Generated random number: ", str(random_number), "\n")
-	return "Number generated and sent back to server\n"
+	return random_number
